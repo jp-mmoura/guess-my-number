a386a8c479c2093160ba284c1ace28d47d3720c4
João Pedro <133026499+jp-mmoura@users.noreply.github.com>
2024-07-19 14:18:52 -0300

    Add files via upload

---
+++ guess-my-number/guess-my-number.js
@@ -0,0 +1,51 @@
+'use strict';
+/*
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20; 
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = document.querySelector('.guess').value;
+    console.log(guess, typeof guess);
+
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'No  Number!'; 
+    } else if(guess == secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number!';
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+    } else if(guess > secretNumber){
+        if(score > 0){
+             document.querySelector('.message').textContent = 'Too high!';
+            score--;
+            document.querySelector('.score').textContent = score; 
+        } else{
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+
+            document.querySelector('body').style.backgroundColor = 'red';
+            document.querySelector('.number').style.width = '30rem';
+        }
+    } else if(guess < secretNumber){
+        if(score > 0){
+            document.querySelector('.message').textContent = 'Too low!'
+            score--; 
+            document.querySelector('.score').textContent = score; 
+        }else{
+            document.querySelector('.message').textContent = 'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+
+            document.querySelector('body').style.backgroundColor = 'red';
+            document.querySelector('.number').style.width = '30rem';
+        }
+    }
+});
